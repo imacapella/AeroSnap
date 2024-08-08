5720a3ed207980e07a399c5f0b8806d08d44dfb5
imacapella <gurkankaradas42@gmail.com>
2024-08-08 14:46:12 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gürkan Karadaş on 8.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
